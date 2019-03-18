08130c2fa90bf417dedfbe81e1b0c83962828ef7
boxp <tiyotiyouda@gmail.com>
2019-03-18 20:10:01 +0900

    Update to latest libraries

--- resources/leiningen/new/spellcard/project.clj
+++ resources/leiningen/new/spellcard/project.clj
@@ -3,15 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.4.474"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/core.async "0.4.490"]
                  [environ "1.1.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [ring "1.6.1"]
+                 [ring "1.7.1"]
                  [ring/ring-json "0.4.0"]
-                 [compojure "1.6.0"]
+                 [compojure "1.6.1"]
                  [cheshire "5.7.1"]
                  [org.clojure/tools.namespace "0.2.10"]]
   :profiles
   {:dev {:source-paths ["src" "dev"]}
    :uberjar {:main {{name}}.system}})
+
